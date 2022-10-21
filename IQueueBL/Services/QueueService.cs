11d9a9c7ad8524f3e5ce2bf8d05fc7da479ae993
Yurii_Zakharov <yurii.zakharov1@nure.ua>
2022-10-21 12:37:40 +0300

    Service implementation

---
+++ IQueueBL/Services/QueueService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using IQueueBL.Interfaces;
+using IQueueBL.Models;
+using IQueueBL.Validation;
+using IQueueData.Entities;
+using IQueueData.Interfaces;
+
+namespace IQueueBL.Services
+{
+    public class QueueService : IQueueService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public QueueService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task AddAsync(QueueModel model)
+        {
+            ValidateQueue(model);
+
+            var queue = _mapper.Map<Queue>(model);
+
+            await _unitOfWork.QueueRepository.AddAsync(queue);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteAsync(Guid modelId)
+        {
+            var model = _mapper.Map<QueueModel>(this.GetByIdAsync(modelId));
+            ValidateQueue(model);            
+
+            var queue = _mapper.Map<Queue>(model);
+
+            _unitOfWork.QueueRepository.Delete(queue);
+            await _unitOfWork.SaveAsync();
+
+        }
+
+        public async Task<IEnumerable<QueueModel>> GetAllAsync()
+        {
+            var queues = await _unitOfWork.QueueRepository.GetAllWithDetailsAsync();
+            return _mapper.Map<IEnumerable<QueueModel>>(queues);
+        }
+
+        public async Task<QueueModel> GetByIdAsync(Guid id)
+        {
+            var queue = await _unitOfWork.QueueRepository.GetByIdAsync(id);
+            return _mapper.Map<QueueModel>(queue);
+        }
+
+        public async Task UpdateAsync(QueueModel model)
+        {
+            ValidateQueue(model);
+
+            var queue = _mapper.Map<Queue>(model);
+
+            _unitOfWork.QueueRepository.Update(queue);
+            await _unitOfWork.SaveAsync();
+        }
+
+        private void ValidateQueue(QueueModel model)
+        {
+            if (string.IsNullOrEmpty(model.CreateTime.ToString()))
+            {
+                throw new QueueException("CreateTime can't be null value.");
+            }
+            if (string.IsNullOrEmpty(model.MaxRecordNumber.ToString()))
+            {
+                throw new QueueException("MaxRecordsNumber can't be null value.");
+            }
+            if (string.IsNullOrEmpty(model.AdminId.ToString()))
+            {
+                throw new QueueException("AdminId can't be null value.");
+            }
+            
+        }
+    }
+}
---
+++ IQueueBL/Services/RecordService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using IQueueBL.Interfaces;
+using IQueueBL.Models;
+using IQueueBL.Validation;
+using IQueueData.Entities;
+using IQueueData.Interfaces;
+
+namespace IQueueBL.Services
+{
+    internal class RecordService : IRecordService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public RecordService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task AddAsync(RecordModel model)
+        {
+            ValidateRecord(model);
+
+            var record = _mapper.Map<QueueRecord>(model);
+
+            await _unitOfWork.RecordRepository.AddAsync(record);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteAsync(Guid modelId)
+        {
+            var model = _mapper.Map<RecordModel>(this.GetByIdAsync(modelId));
+            ValidateRecord(model);
+
+            var record = _mapper.Map<QueueRecord>(model);
+
+            _unitOfWork.RecordRepository.Delete(record);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task<IEnumerable<RecordModel>> GetAllAsync()
+        {
+            var records = await _unitOfWork.RecordRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<RecordModel>>(records);
+        }
+
+        public async Task<RecordModel> GetByIdAsync(Guid id)
+        {
+            var record = await _unitOfWork.RecordRepository.GetByIdAsync(id);
+            return _mapper.Map<RecordModel>(record);
+        }
+
+        public async Task UpdateAsync(RecordModel model)
+        {
+            ValidateRecord(model);
+
+            var record = _mapper.Map<QueueRecord>(model);
+
+            _unitOfWork.RecordRepository.Update(record);
+            await _unitOfWork.SaveAsync();
+        }
+        private void ValidateRecord(RecordModel model)
+        {
+            if (string.IsNullOrEmpty(model.QueueId.ToString()))
+            {
+                throw new QueueException("QueueId can't be null value.");
+            }
+            if (string.IsNullOrEmpty(model.LabNumber.ToString()))
+            {
+                throw new QueueException("LabNumber can't be null value.");
+            }
+            if (string.IsNullOrEmpty(model.Index.ToString()))
+            {
+                throw new QueueException("Index can't be null value.");
+            }
+            if (string.IsNullOrEmpty(model.UserId.ToString()))
+            {
+                throw new QueueException("UserId can't be null value.");
+            }
+
+        }
+    }
+}
